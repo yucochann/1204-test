82d6ff2b597cf7d36b33005df28c895f137fa449
yucochann <yuca.work@gmail.com>
2024-12-04 10:08:40 +0800

    Initial commit

---
+++ vue-project/src/components/__tests__/RegisterView.spec.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+
+describe("當會員資料有缺漏時", () => {
+  it("沒填帳號時，會出現錯誤訊息", () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
